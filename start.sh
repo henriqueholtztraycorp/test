e5b71aa8a6aa232077aa531cfcde4f9b0aa33821
henriqueholtztraycorp <81363452+henriqueholtztraycorp@users.noreply.github.com>
2024-12-19 16:09:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,15 @@
 echo "Starting initial script..."
 echo "pwd: $(pwd)"
 echo "whoami: $(whoami)"
+PROFILE_PATH="/home/$(whoami)/.profile"
+echo "PROFILE_PATH: $PROFILE_PATH"
 
-cat << End >> ~/.profile
-echo "Running my ~/.profile..."
+cat << End >> $PROFILE_PATH
+echo "Running my $PROFILE_PATH..."
 SECRET_TOKEN=abc123!@#
 export SECRET_TOKEN
 End
 
-source ~/.profile
+source $PROFILE_PATH
 echo "$@"
 echo "Finishing initial script..."

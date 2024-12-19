dd278642351616fb28908d866b071b5f1d52c9db
henriqueholtztraycorp <81363452+henriqueholtztraycorp@users.noreply.github.com>
2024-12-19 17:13:53 -0300

    Update start-asgard.sh

--- start-asgard.sh
+++ start-asgard.sh
@@ -7,8 +7,10 @@ echo "PROFILE_PATH: $PROFILE_PATH"
 
 cat << End >> $PROFILE_PATH
 echo "Running my $PROFILE_PATH..."
+NEW_RELIC_LABELS=foo:bar;cluster:ms-homolog2
 SECRET_TOKEN_TEST=abc123!@#
 export SECRET_TOKEN_TEST
+export NEW_RELIC_LABELS
 End
 
 echo "Loading profile..."
@@ -16,6 +18,7 @@ echo "Loading profile..."
 . $PROFILE_PATH
 echo "Profile loaded!"
 echo "env SECRET_TOKEN_TEST: $(printenv SECRET_TOKEN_TEST)"
+echo "env NEW_RELIC_LABELS: $(printenv NEW_RELIC_LABELS)"
 
 echo "command: $@"
 

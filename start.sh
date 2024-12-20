e9067fb73e4de8904775466c42bfb84b51c00ad1
henriqueholtztraycorp <81363452+henriqueholtztraycorp@users.noreply.github.com>
2024-12-20 15:49:58 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,15 +6,16 @@ PROFILE_PATH="/home/$(whoami)/.profile"
 echo "PROFILE_PATH: $PROFILE_PATH"
 CLUSTER_NAME=$(curl --connect-timeout 2.0 -s ${ECS_CONTAINER_METADATA_URI_V4}/task | grep -o '"Cluster":"[^"]*"' | cut -d':' -f2 | tr -d '"')
 echo "CLUSTER_NAME: $CLUSTER_NAME"
-echo "HOSTNAME: $HOSTNAME"
+echo "HOSTNAME before: $HOSTNAME"
+NEW_RELIC_PROCESS_HOST_DISPLAY_NAME="$CLUSTER_NAME-$HOSTNAME"
+NEW_RELIC_LABELS="foo:bar;cluster:$CLUSTER_NAME"
 
 cat << End >> $PROFILE_PATH
 echo "Running my $PROFILE_PATH..."
-NEW_RELIC_LABELS="foo:bar;cluster:$CLUSTER_NAME"
-NEW_RELIC_PROCESS_HOST_DISPLAY_NAME="$CLUSTER_NAME-$HOSTNAME"
 export NEW_RELIC_LABELS
 export CLUSTER_NAME
 export NEW_RELIC_PROCESS_HOST_DISPLAY_NAME
+export HOSTNAME="$NEW_RELIC_PROCESS_HOST_DISPLAY_NAME"
 End
 
 echo "Loading profile..."
@@ -23,6 +24,7 @@ echo "Loading profile..."
 echo "Profile loaded!"
 echo "env NEW_RELIC_LABELS: $(printenv NEW_RELIC_LABELS)"
 echo "env NEW_RELIC_PROCESS_HOST_DISPLAY_NAME: $(printenv NEW_RELIC_PROCESS_HOST_DISPLAY_NAME)"
+echo "HOSTNAME after: $HOSTNAME"
 
 echo "command: $@"
 

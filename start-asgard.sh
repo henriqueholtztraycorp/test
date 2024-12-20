d82a359969cb1072d40d7c7183a4c6a57e5f5c8f
henriqueholtztraycorp <81363452+henriqueholtztraycorp@users.noreply.github.com>
2024-12-20 08:37:32 -0300

    Update start-asgard.sh

--- start-asgard.sh
+++ start-asgard.sh
@@ -4,20 +4,20 @@ echo "pwd: $(pwd)"
 echo "whoami: $(whoami)"
 PROFILE_PATH="/home/$(whoami)/.profile"
 echo "PROFILE_PATH: $PROFILE_PATH"
+CLUSTER_NAME=$(curl -s ${ECS_CONTAINER_METADATA_URI_V4}/task | grep -o '"Cluster":"[^"]*"' | cut -d':' -f2 | tr -d '"')
+echo "CLUSTER_NAME: $CLUSTER_NAME"
 
 cat << End >> $PROFILE_PATH
 echo "Running my $PROFILE_PATH..."
-NEW_RELIC_LABELS="foo:bar;cluster:ms-homolog2"
-SECRET_TOKEN_TEST=abc123!@#
-export SECRET_TOKEN_TEST
+NEW_RELIC_LABELS="foo:bar;cluster:$CLUSTER_NAME"
 export NEW_RELIC_LABELS
+export CLUSTER_NAME
 End
 
 echo "Loading profile..."
 # source $PROFILE_PATH => It doesn't work because some shell uses just "." instead of "source"
 . $PROFILE_PATH
 echo "Profile loaded!"
-echo "env SECRET_TOKEN_TEST: $(printenv SECRET_TOKEN_TEST)"
 echo "env NEW_RELIC_LABELS: $(printenv NEW_RELIC_LABELS)"
 
 echo "command: $@"
